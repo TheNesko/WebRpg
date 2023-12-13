c9e09ec4f87ef937c5127ce4884b7b705ed06689
Czar0pl <skrimuofficial@gmail.com>
2023-12-13 19:54:33 +0100

    fttfy

---
+++ public/TEST/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logowanie</title>
+
+    <style>
+        h1{
+            color:red;
+        }
+    </style>
+
+</head>
+<body>
+    <?php
+        $NDKSJC="DCDSC";
+
+    ?>
+
+    <h1>Moja strona w php</h1>
+
+    <ul>
+        <li>
+            <a href="/?action=logowanie">1</a>
+        </li>
+        <li>
+            <a href="/?action=stronaglowna">2</a>
+        </li>
+        <li>
+            <a href="/?action=rejestrowanie">3</a>
+        </li>
+    </ul>
+</body>
+</html>
